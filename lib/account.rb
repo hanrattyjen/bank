d4d306f8e56d7fb969819e7d269eedc094b34e13
hanrattyjen <hanratty.jen@gmail.com>
2016-11-29 13:27:25 +0000

    Add steps to do to save each transaction to a file.

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,5 @@
 require 'Date'
+require 'csv'
 
 class Account
 
@@ -12,21 +13,32 @@ class Account
 
   def deposit(amount)
     @balance += amount
+    # need this transaction as an array of date, credit and amount
+      #convert_transaction_array_to_string = array.join(",")
+    # save this transaction to a file
+      # call to save_to_file method
   end
 
   def withdraw(amount)
     fail "Cannot withdraw. You have no money fool!" if (@balance - amount < 0)
     @balance -= amount
+    # need this transaction as an array of date, debit and amount
+      #convert_transaction_array_to_string = array.join(",")
+    # save this transaction to a file
+    # call to save_to_file method
+  end
+
+  def print_statement(array)
+    # loads the file
+    # formats it nicely
   end
 
   def save_to_file
-
+    CSV.open("account", "w") do |transaction_row|
+      transaction_row << convert_transaction_array_to_string
+    end
   end
 end
-
-
-
-
 
   # transaction
     # date
