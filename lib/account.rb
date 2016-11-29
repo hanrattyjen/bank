7fa1b3ad35e8df57619ea6317cbc163ab455ea03
hanrattyjen <hanratty.jen@gmail.com>
2016-11-29 13:51:33 +0000

    Transactions are saving to account.csv.
    Overriding each other everytime new transaction is loaded. Need to fix.

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,4 @@
-require 'Date'
+require 'date'
 require 'csv'
 
 class Account
@@ -14,8 +14,11 @@ class Account
   def deposit(amount)
     @balance += amount
     # need this transaction as an array of date, credit and amount
+    credit_array = [Date.today, 0, amount, @balance]
       #convert_transaction_array_to_string = array.join(",")
+    credit_array.join(",")
     # save this transaction to a file
+    save_to_file(credit_array)
       # call to save_to_file method
   end
 
@@ -23,19 +26,24 @@ class Account
     fail "Cannot withdraw. You have no money fool!" if (@balance - amount < 0)
     @balance -= amount
     # need this transaction as an array of date, debit and amount
+    debit_array = [Date.today, amount, 0, @balance]
       #convert_transaction_array_to_string = array.join(",")
+    debit_array.join(",")
     # save this transaction to a file
+    save_to_file(debit_array)
     # call to save_to_file method
   end
 
   def print_statement(array)
     # loads the file
     # formats it nicely
+    line_width = 50
+    puts date.ljust(line_width) + " | " + credit.center(line_width) + " | " + debit.center(line_width) + " | " + balance.center(line_width) + " |"
   end
 
-  def save_to_file
+  def save_to_file(string)
     CSV.open("account", "w") do |transaction_row|
-      transaction_row << convert_transaction_array_to_string
+      transaction_row << string
     end
   end
 end
